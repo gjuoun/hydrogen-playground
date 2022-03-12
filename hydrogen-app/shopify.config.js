2c0a1e31d483c8759fcb8b7e10b387b9f37f11c5
Jun Guo <gjuoun@gmail.com>
2022-03-12 02:43:04 -0400

    update new project

--- hydrogen-app/shopify.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  locale: 'en-us',
-  storeDomain: 'shrimp-lover-and-tropical-fish.myshopify.com',
-  storefrontToken: 'bcf24ec7a33c26e31c6485c7b6beebeb',
-  graphqlApiVersion: 'unstable',
-};
